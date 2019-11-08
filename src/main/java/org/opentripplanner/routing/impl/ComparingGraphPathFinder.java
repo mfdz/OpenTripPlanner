93d33fda5fc8f3ffbae37b2fead35afb03e309e6
Leonard Ehrenfried <mail@leonard.io>
2019-11-08 16:41:15 +0100

    Run requests in parallel again

--- src/main/java/org/opentripplanner/routing/impl/ComparingGraphPathFinder.java
+++ src/main/java/org/opentripplanner/routing/impl/ComparingGraphPathFinder.java
@@ -2,8 +2,6 @@ package org.opentripplanner.routing.impl;
 
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
-import org.opentripplanner.api.model.TripPlan;
-import org.opentripplanner.api.resource.GraphPathToTripPlanConverter;
 import org.opentripplanner.routing.core.RoutingRequest;
 import org.opentripplanner.routing.core.TraverseMode;
 import org.opentripplanner.routing.error.PathNotFoundException;
@@ -12,9 +10,10 @@ import org.opentripplanner.standalone.Router;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * This variant of the GraphPathFinder "fans out" a routing request into potentially multiple ones and compares the
@@ -32,35 +31,41 @@ public class ComparingGraphPathFinder extends GraphPathFinder {
     }
 
     @Override
-    public List<GraphPath> graphPathFinderEntryPoint(RoutingRequest options) {
+    public List<GraphPath> graphPathFinderEntryPoint(RoutingRequest originalRequest) {
 
-        List<GraphPath> results;
-        if (options.parkAndRide) {
+        if (originalRequest.parkAndRide) {
             LOG.debug("Detected a P&R routing request. Will execute two requests to also get car-only routes.");
 
             // in order to avoid race conditions we have to clone beforehand
-            RoutingRequest clone = options.clone();
+            RoutingRequest clone = originalRequest.clone();
             // the normal P&R
-            List<GraphPath> parkAndRide = runParkAndRideRequest(clone);
-            // car-only
-            List<GraphPath> carOnly = runCarOnlyRequest(options);
+            CompletableFuture<List<GraphPath>> carOnlyF = CompletableFuture.supplyAsync(() -> runCarOnlyRequest(clone));
+            // the normal P&R
+            CompletableFuture<List<GraphPath>> parkAndRideF = CompletableFuture.supplyAsync(() -> runParkAndRideRequest(originalRequest));
+            // the CompletableFutures are there to make sure that the computations run in parallel
+            List<List<GraphPath>> allResults = Stream.of(parkAndRideF, carOnlyF)
+                    .map(CompletableFuture::join)
+                    .collect(Collectors.toList());
 
-            results = filterOut(parkAndRide, carOnly);
+            List<GraphPath> parkAndRide = allResults.get(0);
+            List<GraphPath> carOnly = allResults.get(1);
+
+            List<GraphPath> results = filterOut(parkAndRide, carOnly);
 
             if(results == null || results.isEmpty()) {
                 throw new PathNotFoundException();
             }
+            return results;
 
         } else {
-            results = super.graphPathFinderEntryPoint(options);
+            return super.graphPathFinderEntryPoint(originalRequest);
         }
 
-        return results;
     }
 
-    private List<GraphPath> runParkAndRideRequest(RoutingRequest clone) {
+    private List<GraphPath> runParkAndRideRequest(RoutingRequest request) {
         try {
-            return super.graphPathFinderEntryPoint(clone);
+            return super.graphPathFinderEntryPoint(request);
         } catch (PathNotFoundException e) {
             LOG.debug("Could not find park & ride trips.", e);
             // we don't need to call cleanup here because it is called in PlannerResource/GraphQLResource
