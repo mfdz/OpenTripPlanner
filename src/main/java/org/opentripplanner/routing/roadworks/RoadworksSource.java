fa1dee0e1384c766fabaa9b64257a3e3c704e587
Leonard Ehrenfried <mail@leonard.io>
2020-01-17 12:17:13 +0100

    Set up test case for roadworks

--- src/main/java/org/opentripplanner/routing/roadworks/RoadworksSource.java
+++ src/main/java/org/opentripplanner/routing/roadworks/RoadworksSource.java
@@ -1,10 +1,23 @@
 package org.opentripplanner.routing.roadworks;
 
+import com.google.common.collect.Sets;
 import org.opentripplanner.routing.edgetype.StreetEdge;
+
+import java.util.*;
 
 public class RoadworksSource {
 
+    public RoadworksSource() {
+        this.blockedEdgeIds = Sets.newHashSet();
+    }
+
+    public RoadworksSource(Set<Integer> blockedEdgeIds) {
+        this.blockedEdgeIds = blockedEdgeIds;
+    }
+
+    private Set<Integer> blockedEdgeIds;
+
     public boolean isBlocked(StreetEdge edge){
-       return false;
+      return blockedEdgeIds.contains(edge.getId());
     }
 }
