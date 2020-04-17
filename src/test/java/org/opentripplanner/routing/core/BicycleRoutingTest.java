37bb7e13dba1c96feb0d06cb3a08cdde5cce5fb8
Leonard Ehrenfried <mail@leonard.io>
2020-04-17 14:49:50 +0200

    Finetune the German way property set for cycling
    Previously the local, regional and national cycle networks
    were heavily favoured. This works quite well when routing from
    town to town.
    
    However, inside cities this would sometimes lead to very long
    detours as a route via the cycling network was selected instead.
    
    In this commit I decreased the safety factor of the regional cycling
    networks a little and increased the safety of residential roads with
    a speed limit.
    
    Fixes https://github.com/mfdz/digitransit-ui/issues/127

--- src/test/java/org/opentripplanner/routing/core/BicycleRoutingTest.java
+++ src/test/java/org/opentripplanner/routing/core/BicycleRoutingTest.java
@@ -22,7 +22,11 @@ public class BicycleRoutingTest {
     static Graph graph;
 
     static GenericLocation nebringen = new GenericLocation(48.56494, 8.85318);
-    static GenericLocation herrenbergBahnhof = new GenericLocation(48.59345, 8.86245);
+    static GenericLocation herrenbergMarketSquare = new GenericLocation(48.59634, 8.87015);
+
+    static GenericLocation herrenbergErhardtstBismarckstr = new GenericLocation(48.59247, 8.86811);
+    static GenericLocation herrenbergMarkusstrMarienstr = new GenericLocation(48.59329, 8.87253);
+
     static long dateTime = TestUtils.dateInSeconds("Europe/Berlin", 2020, 03, 10, 11, 0, 0);
 
     @BeforeClass
@@ -30,33 +34,36 @@ public class BicycleRoutingTest {
         graph = TestGraphBuilder.buildGraph(ConstantsForTests.NEBRINGEN_HERRENBERG_OSM);
     }
 
-    private static RoutingRequest buildRoutingRequest(Graph graph) {
+    private static String getPolyline(Graph graph, GenericLocation from, GenericLocation to) {
         RoutingRequest request = new RoutingRequest();
         request.dateTime = dateTime;
-        request.from = nebringen;
-        request.to = herrenbergBahnhof;
+        request.from = from;
+        request.to = to;
 
         request.modes = new TraverseModeSet(TraverseMode.BICYCLE);
 
         request.setNumItineraries(1);
         request.setRoutingContext(graph);
-        request.setOptimize(OptimizeType.TRIANGLE);
-        request.setTriangleSafetyFactor(0.5);
-        request.setTriangleSlopeFactor(0.5);
-        request.setTriangleTimeFactor(0);
+        request.setOptimize(OptimizeType.GREENWAYS); // this is the default setting in digitransit
 
-        return request;
+        GraphPathFinder gpf = new GraphPathFinder(new Router(graph.routerId, graph));
+        List<GraphPath> paths = gpf.graphPathFinderEntryPoint(request);
+
+        TripPlan plan = GraphPathToTripPlanConverter.generatePlan(paths, request);
+        return plan.itinerary.get(0).legs.get(0).legGeometry.getPoints();
     }
 
     @Test
     public void useBikeNetworkRoutesFromNebringenToHerrenberg() {
-        RoutingRequest req = buildRoutingRequest(graph);
-        GraphPathFinder gpf = new GraphPathFinder(new Router(graph.routerId, graph));
-        List<GraphPath> paths = gpf.graphPathFinderEntryPoint(req);
-
-        TripPlan plan = GraphPathToTripPlanConverter.generatePlan(paths, req);
-        String polyline = plan.itinerary.get(0).legs.get(0).legGeometry.getPoints();
-        assertThat(polyline, is("{ilgHgc`u@IGC[y@?@qAZaCAKEMGKSCe@CWEOe@GGgCoNa@mEWqCo@eHI_Ay@wIYeDo@kHe@yEu@gGm@aFWmBkHgEKCeC\\gFn@M_@NoAWAOGSWRi@zAsFYMIOc@aAEIo@yAGIG\\IZ]p@e@v@u@|@m@f@cCn@?pBGt@Kx@[fB]bBYx@c@`Ag@x@g@b@S@YEa@Ge@OeBw@eAa@a@Cc@JiBv@_@P]FS@o@KsAg@qAe@e@Co@HyBh@MFMHKLEPCLi@Po@PqAT_@N_Ad@c@Nk@LYJ]JU@i@?w@Cg@?G@K?G??[COGOkAwBs@cBK]]oAa@mAKu@]{ByAtAULg@Hq@Aq@EsCQa@CC?qB@]BkAJ_C^eCCqBGP|AkA\\IVw@Ry@L_CZDVcB`@wA`@iBh@?ByBv@oCt@q@PQFY`@Wp@CJKZ]dANLERJ\\Ft@@Z@d@VlABLh@zBRz@VdABR@LNj@Rv@Df@?B@D"));
+        var polyline = getPolyline(graph, nebringen, herrenbergMarketSquare);
+        // map: https://gist.github.com/leonardehrenfried/00bc74682275a8b941b1623f383a8fb4
+        assertThat(polyline, is("{ilgHgc`u@IGC[y@?@qAZaCAKEMGKSCe@CWEOe@GGgCoNa@mEWqCo@eHI_Ay@wIYeDo@kHe@yEu@gGm@aFWmBkHgEKCeC\\gFn@M_@NoAWAOGSWRi@zAsFYMIOc@aAEIo@yAGIG\\IZ]p@e@v@u@|@m@f@cCn@?pBGt@Kx@[fB]bBYx@c@`Ag@x@g@b@S@YEa@Ge@OeBw@eAa@a@Cc@JiBv@_@P]FS@o@KsAg@qAe@e@Co@HyBh@MFMHKLEPCLi@Po@PqAT_@N_Ad@c@Nk@LYJ]JU@i@?w@Cg@?G@K?G??[COGOkAwBs@cBK]]oAa@mAKu@]{ByAtAULg@Hq@Aq@EsCQa@CC?qB@]BkAJ_C^eCCqBGqAE[[o@GM_DQ_EK{Be@wCm@wDAGqB`AwB`AmB|@GDg@~@e@l@y@t@e@Rk@JsCh@@D?J@LWDGBAHKF?HSHG~@G?MC_@EAx@Gl@YZMb@CH?N?r@"));
     }
 
+    @Test
+    public void dontUseCycleNetworkInsideHerrenberg() {
+        var polyline = getPolyline(graph, herrenbergErhardtstBismarckstr, herrenbergMarkusstrMarienstr);
+        // map: https://gist.github.com/leonardehrenfried/89dd1438714c3e95a63807908b2cb27a
+        assertThat(polyline, is("}uqgHs`cu@AK?Cm@iEIo@?eD]UFe@c@iD]kB_@sAACCIRQ"));
+    }
 }
