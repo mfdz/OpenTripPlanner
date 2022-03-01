cec135b25bbd6b1d7c8c6c51bc85ac62a0f9a7e5
Leonard Ehrenfried <mail@leonard.io>
2022-03-01 09:28:59 +0100

    Use Bike mode for P&R

--- src/main/java/org/opentripplanner/api/parameter/QualifiedModeSet.java
+++ src/main/java/org/opentripplanner/api/parameter/QualifiedModeSet.java
@@ -126,7 +126,7 @@ public class QualifiedModeSet implements Serializable {
                         egressMode = StreetMode.WALK;
                         // CAR_TO_PARK is quite expensive to compute, so we only use it if there are no
                         // transit modes
-                        directMode = transitModes.isEmpty() ? StreetMode.CAR_TO_PARK : StreetMode.CAR;
+                        directMode = transitModes.isEmpty() ? StreetMode.CAR_TO_PARK : StreetMode.BIKE;
                     } else if (requestMode.qualifiers.contains(Qualifier.PICKUP)) {
                         accessMode = StreetMode.WALK;
                         transferMode = StreetMode.WALK;
