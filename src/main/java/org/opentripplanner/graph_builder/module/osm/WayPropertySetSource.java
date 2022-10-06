1adb2ef64a0e6bb6d4aa0d1010f286354899122b
Leonard Ehrenfried <mail@leonard.io>
2022-10-06 10:08:26 +0200

    Format code

--- src/main/java/org/opentripplanner/graph_builder/module/osm/WayPropertySetSource.java
+++ src/main/java/org/opentripplanner/graph_builder/module/osm/WayPropertySetSource.java
@@ -48,10 +48,7 @@ public interface WayPropertySetSource {
   }
 
   default boolean doesTagValueAllowThroughTraffic(String tagValue) {
-    return (
-      "yes".equals(tagValue) ||
-      "designated".equals(tagValue)
-    );
+    return ("yes".equals(tagValue) || "designated".equals(tagValue));
   }
 
   default boolean isGeneralNoThroughTraffic(OSMWithTags way) {
@@ -81,11 +78,13 @@ public interface WayPropertySetSource {
   default boolean isBicycleNoThroughTrafficExplicitlyDisallowed(OSMWithTags way) {
     String bicycle = way.getTag("bicycle");
     return (
-      (isVehicleThroughTrafficExplicitlyDisallowed(way) &&
+      (
+        isVehicleThroughTrafficExplicitlyDisallowed(way) &&
         // NOTE: specific bicycle tag explicitly overrides access/vehicle tags
         // see https://wiki.openstreetmap.org/wiki/Tag:vehicle%3Ddestination
-        !doesTagValueAllowThroughTraffic(bicycle)) ||
-        doesTagValueDisallowThroughTraffic(bicycle)
+        !doesTagValueAllowThroughTraffic(bicycle)
+      ) ||
+      doesTagValueDisallowThroughTraffic(bicycle)
     );
   }
 
